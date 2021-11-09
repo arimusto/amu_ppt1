d8f42cc308ffb826c24c66d17b1b5a5dba00ad1e
Ari Mustonen <a.mustonen@gmail.com>
2021-11-09 16:15:21 +0200

    Server valmis

---
+++ server.Dockerfile
@@ -0,0 +1,10 @@
+# syntax=docker/dockerfile:1
+FROM python:3.7-alpine
+WORKDIR /serverdata
+ENV FLASK_APP=runserver.py
+ENV FLASK_RUN_HOST=0.0.0.0
+COPY server.requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+EXPOSE 5000
+COPY . .
+CMD ["flask", "run"]
---
+++ servervol/runserver.py
@@ -0,0 +1,34 @@
+from random import choice
+from string import ascii_letters as ltrs
+from hashlib import md5
+from flask import Flask, send_file
+
+md5hash = md5()
+
+with open("file.txt", "w", encoding = "utf-8") as file:
+    random_text = ""
+    for _ in range(1024):
+        random_text += choice(ltrs)
+    file.write(random_text)
+
+with open("file.txt", "rb") as file:
+    content = file.read()
+    md5hash.update(content)
+
+with open("md5hash.txt", "w", encoding = "utf-8") as file:
+    file.write(md5hash.hexdigest())
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return send_file("./hello.html")
+
+@app.route("/data")
+def data():
+    return send_file("./file.txt")
+
+@app.route("/hash")
+def hash():
+    return send_file("./md5hash.txt")
+
