cbc91c520db39fd5e8533e4fc4b8430f17032722
Ari Mustonen <a.mustonen@gmail.com>
2021-11-10 13:13:35 +0200

    Toimiva versio 1.0

--- clientvol/runclient.py
+++ clientvol/runclient.py
@@ -1,16 +1,50 @@
-from random import choice
-from string import ascii_letters as ltrs
+from urllib import request
 from hashlib import md5
 from flask import Flask, send_file
 
-with open("file.txt", "w", encoding = "utf-8") as file:
-    random_text = ""
-    for _ in range(1024):
-        random_text += choice(ltrs)
-    file.write(random_text)
+data_url = "http://server.docker:5000/data"
+hash_url = "http://server.docker:5000/hash"
+filename = "./file.txt"
+
+def get_file(fname: str):
+    try:
+        with request.urlopen(data_url) as data:
+            data = data.read().decode("utf-8")
+        with request.urlopen(hash_url) as hash:
+            hash = hash.read().decode("utf-8")
+    except:
+        return False
+
+    with open(fname, "w", encoding = "utf-8") as file:
+        file.write(data)
+    
+    return (data, hash)
+
+def get_hash(fname: str):
+    md5hash = md5()
+    with open(fname, "rb") as file:
+        content = file.read()
+        md5hash.update(content)
+
+    return md5hash.hexdigest()    
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return send_file("./hello.html")
+    data = get_file(filename)
+    if not data:
+        return send_file("./error.html")
+
+    checksum = get_hash(filename)
+
+    page = ""
+    page += "<!DOCTYPE html>\n<html lang='en'>\n<head>\n<meta charset='UTF-8'>\n<title>Hello from Client</title>\n</head>\n<body>\n"
+    page += "<p>Palvelimelta noudettu data:</p>\n"
+    page += f"<p>{data[0]}</p>\n"
+    page += f"<p>Data tallennettu tiedostoon '{filename}'</p>\n"
+    page += f"<p>Palvelimelta saatu tarkastussumma: {data[1]}</p>\n"
+    page += f"<p>Paikallisen tiedoston tarkastussumma: {checksum}</p>\n"
+    page += "</body>\n</html>\n"
+    return page
--- servervol/runserver.py
+++ servervol/runserver.py
@@ -3,19 +3,23 @@ from string import ascii_letters as ltrs
 from hashlib import md5
 from flask import Flask, send_file
 
-def create_files():
-    md5hash = md5()
-    with open("file.txt", "w", encoding = "utf-8") as file:
+filename = "./file.txt"
+hashname = "./md5hash.txt"
+
+def create_file(fname: str):
+    with open(fname, "w", encoding = "utf-8") as file:
         random_text = ""
         for _ in range(1024):
             random_text += choice(ltrs)
         file.write(random_text)
 
-    with open("file.txt", "rb") as file:
+def create_hash(fname: str, hname: str):
+    md5hash = md5()
+    with open(fname, "rb") as file:
         content = file.read()
         md5hash.update(content)
 
-    with open("md5hash.txt", "w", encoding = "utf-8") as file:
+    with open(hname, "w", encoding = "utf-8") as file:
         file.write(md5hash.hexdigest())
 
 app = Flask(__name__)
@@ -26,9 +30,10 @@ def hello():
 
 @app.route("/data")
 def data():
-    create_files()
-    return send_file("./file.txt")
+    create_file(filename)
+    create_hash(filename, hashname)
+    return send_file(filename)
 
 @app.route("/hash")
 def hash():
-    return send_file("./md5hash.txt")
+    return send_file(hashname)
